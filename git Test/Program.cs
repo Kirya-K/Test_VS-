e9504cc840a5a8c8f0cb13f1ef351ef3162c30b2
Оля Кириченко <OlyaKir2001@mail.ru>
2020-12-25 16:33:02 +0700

    Уможение

--- git Test/Program.cs
+++ git Test/Program.cs
@@ -6,7 +6,14 @@ namespace git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите А:");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите B:");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int e = a * b;
+            Console.WriteLine("=" + e);
         }
     }
 }
